6d2c92d5d3cb9dc3fec8446f435c447dd47c6bbe
AlgoClassWork <kyxec.play@gmail.com>
2025-07-16 12:53:51 +0600

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField('Заголовок', max_length=200)
+    content = models.TextField('Содержание')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    image = models.ImageField('Изображение', upload_to='post_images/', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
