31c3552750ef46af1599a914e12102726e1ef3da
AlgoClassWork <kyxec.play@gmail.com>
2025-07-17 13:20:05 +0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
@@ -8,3 +8,10 @@ def post_list(request):
     posts = Post.objects.all()
     context = {'posts' : posts}
     return render(request, 'post_list.html', context )
+
+# http://127.0.0.1:8000/detail/3
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    context = {'post' : post}
+    return render(request, 'post_detail.html', context )
+
