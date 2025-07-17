ec6635136c4723b72c02ae7d2837a38b32afc6d0
AlgoClassWork <kyxec.play@gmail.com>
2025-07-17 12:21:35 +0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
 # http://127.0.0.1:8000/
 def post_list(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.all()
+    context = {'posts' : posts}
+    return render(request, 'post_list.html', context )
